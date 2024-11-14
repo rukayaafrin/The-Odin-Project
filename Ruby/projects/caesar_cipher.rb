f03e5faf26db317f581381e71866f1cfb495492a
rukayaafrin <e0639627@u.nus.edu>
2024-11-14 08:18:33 +0800

    To do: Ruby questions

---
+++ Ruby/projects/caesar_cipher.rb
@@ -0,0 +1,5 @@
+=begin
+with a left shift of 3, D would be replaced by A, E would become B, and so on.
+> caesar_cipher("What a string!", 5)
+  => "Bmfy f xywnsl!"
+=end
---
+++ Ruby/projects/sub_string.rb
@@ -0,0 +1,11 @@
+=begin
+Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument.
+  > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  > substrings("below", dictionary)
+  => { "below" => 1, "low" => 1 }
+
+> substrings("Howdy partner, sit down! How's it going?", dictionary)
+  => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+=end
